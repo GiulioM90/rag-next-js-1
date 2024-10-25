c43f663165d94bdde2c2825c873f5c46149e8af8
GiulioM90 <giuliomollo90@gmail.com>
2024-10-25 14:13:14 +0200

    added text and pdf loader from langchain

--- pages/api/updatedatabase.ts
+++ pages/api/updatedatabase.ts
@@ -1,3 +1,6 @@
+import { DirectoryLoader } from "langchain/document_loaders/fs/directory";
+import { TextLoader } from "langchain/document_loaders/fs/text";
+import { PDFLoader } from "@langchain/community/document_loaders/fs/pdf";
 import { NextApiRequest, NextApiResponse } from "next";
 
 const postDB =  async (req:NextApiRequest, res: NextApiResponse) => {
@@ -8,7 +11,12 @@ const postDB =  async (req:NextApiRequest, res: NextApiResponse) => {
 }
 
 const handleUpload = (indexname : string, namespace: string, res: NextApiResponse) => {
-
+  const loader = new DirectoryLoader('./documents', {
+    '.pdf': (path: string) => new PDFLoader(path, {
+      splitPages: false
+    }),
+    '.txt': (path: string) => new TextLoader(path)
+  })
 }
 
 export default postDB
